37485c7f818bb7da34e192b4ba5d3d619a629d2c
Niklas Lindblad <niklas.lindblad@guardian.co.uk>
2015-09-18 14:20:36 +0100

    Fix path and output

--- cloudformation/validate-cloudformation.sh
+++ cloudformation/validate-cloudformation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CLOUDFORMATION_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/cloudformation"
+CLOUDFORMATION_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 which aws 1>/dev/null 2>&1
 if [[ $? -gt 0 ]]; then
@@ -10,10 +10,11 @@ fi
 
 if [[ -d "${CLOUDFORMATION_DIRECTORY}" ]]; then
 	for TEMPLATE in $(find ${CLOUDFORMATION_DIRECTORY} -iname "*.json"); do
-		echo "Validating CloudFormation template ${TEMPLATE}"
+		echo -n "Validating CloudFormation template ${TEMPLATE}..."
 		aws cloudformation validate-template --template-body "file://${TEMPLATE}" 1>/dev/null
 		if [[ $? -gt 0 ]]; then
 			exit 1
 		fi
+		echo " OK"
 	done
 fi
