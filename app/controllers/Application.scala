b6a44e18b89ca1da202a555d24f2142324c11725
Susie Coleman <susie.coleman@guardian.co.uk>
2015-09-25 15:25:21 +0100

    Added log for when index page is hit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,12 @@
 package controllers
 
+import play.api.Logger
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
+    Logger.info("Index page hit.")
     Ok(views.html.index())
   }
 
