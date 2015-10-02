1c69bf8b8187cec1b72319fc40103455d0fe75dc
Emmanuelle Poirier <emmanuelle.poirier@guardian.co.uk>
2015-10-02 15:50:28 +0100

    Put back index logging

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,12 @@
 package controllers
 
 import play.api.mvc._
+import util.Logging
 
-object Application extends Controller with AuthActions {
+object Application extends Controller with AuthActions with Logging {
 
   def index = AuthAction {
+    logger.info("Index page hit.")
     Ok(views.html.index())
   }
 
