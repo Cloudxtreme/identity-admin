7b6da7fd01e9492269b0f46aa57dc233fa164ac4
Susie Coleman <susie.coleman@guardian.co.uk>
2015-10-12 12:02:14 +0100

    Updated to use DateTime class for dates and times as opposed to having them as strings.

--- app/models/SearchResult.scala
+++ app/models/SearchResult.scala
@@ -1,13 +1,15 @@
 package models
 
+import org.joda.time.DateTime
+
 case class SearchResult(email: String, username: String, name: String,
-                        registeredDate: String, lastActivityDate: String)
+                        registeredDate: DateTime, lastActivityDate: DateTime)
 
 object SearchResult {
 
   def all(): List[SearchResult] =
     Nil
 
-  def mockData(): SearchResult = new SearchResult("email@email.com", "myUsername", "FirstName Surname", "05 Apr 2015",
-    "06 Apr 2015 12:30:31")
+  def mockData(): SearchResult = new SearchResult("email@email.com", "myUsername", "FirstName Surname", new DateTime(2015, 4, 15, 16, 12, 0),
+    new DateTime(2015, 4, 15, 16, 12, 0))
 }
