39e0911fc0811718ef236b17ce5c9f73742c8195
markjamesbutler <markjamesbutler@gmail.com>
2015-10-12 16:24:04 +0100

    Added ordering test.

--- test/services/GoogleGroups$Test.scala
+++ test/services/GoogleGroups$Test.scala
@@ -19,6 +19,7 @@ class GoogleGroups$Test extends FlatSpec with Matchers with ScalaFutures with In
   it should "determine if user is in correct groups" in {
     val required = Set("2FA_enforce","useradmin")
     GoogleGroups.isAuthorised(required, Set("2FA_enforce","useradmin")) should be (true)
+    GoogleGroups.isAuthorised(required, Set("useradmin","2FA_enforce")) should be (true)
     GoogleGroups.isAuthorised(required, Set("2FA_enforce","useradmin","testgroup")) should be (true)
     GoogleGroups.isAuthorised(required, Set("2FA_enforce")) should be (false)
   }
