d3965c7a6c53a23e262e5155c078e61f392eaa38
markjamesbutler <markjamesbutler@gmail.com>
2015-10-13 16:35:56 +0100

    Added missing tests.

---
+++ test/services/Authorisation$Test.scala
@@ -0,0 +1,16 @@
+package services
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+
+class Authorisation$Test extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
+
+  it should "determine if user is in correct groups" in {
+    val required = Set("2FA_enforce","useradmin")
+    Authorisation.isAuthorised(required, Set("2FA_enforce","useradmin")) should be (true)
+    Authorisation.isAuthorised(required, Set("useradmin","2FA_enforce")) should be (true)
+    Authorisation.isAuthorised(required, Set("2FA_enforce","useradmin","testgroup")) should be (true)
+    Authorisation.isAuthorised(required, Set("2FA_enforce")) should be (false)
+  }
+}
