2c4a49fc727d926f1db3b19fe876a7cc89b96ac6
Susie Coleman <susie.coleman@guardian.co.uk>
2015-10-14 14:18:10 +0100

    Removed hardcoded parameters from index view.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,9 @@
 package controllers
 
 import models.SearchResult
+import play.api.i18n.Messages
+import play.api.i18n.Messages.Implicits._
+import play.api.Play.current
 import play.api.mvc._
 import util.Logging
 
@@ -8,7 +11,7 @@ object Application extends Controller with AuthActions with Logging {
 
   def index = AuthAction {
     logger.info("Index page hit.")
-    Ok(views.html.index())
+    Ok(views.html.index(Messages("index.title"), Messages("index.searchBarText")))
   }
 
   def search(searchQuery: String) = Action {
