6ae041f33b2d17e44e1765b33f1d522d3d362b80
Susie Coleman <susie.coleman@guardian.co.uk>
2015-10-16 12:29:42 +0100

    Removed unneccesary comment and import.

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -1,6 +1,5 @@
 package controllers
 
-import controllers.Application._
 import models.SearchResult
 import play.api.i18n.Messages
 import play.api.i18n.Messages.Implicits._
@@ -8,9 +7,6 @@ import play.api.Play.current
 import play.api.mvc.{Session, RequestHeader, Controller}
 import util.Logging
 
-/**
- * Created by scoleman on 16/10/15.
- */
 object Search extends Controller with AuthActions with Logging{
 
   def search(searchQuery: String) = AuthAction {
