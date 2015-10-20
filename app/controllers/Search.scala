ecf91ce254796e07852ddfa6f10e0757c5739827
Susie Coleman <susie.coleman@guardian.co.uk>
2015-10-20 11:38:03 +0100

    Included response from API in view.

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -1,15 +1,14 @@
 package controllers
 
-import models.{UserSummary, SearchResponse, SearchResult}
+import models.SearchResponse
 import play.api.i18n.Messages
 import play.api.i18n.Messages.Implicits._
 import play.api.Play.current
-import play.api.libs.json.{JsArray, Reads, Json}
-import play.api.mvc.{Session, RequestHeader, Controller}
+import play.api.libs.json.Json
+import play.api.mvc.Controller
 import services.AdminApi
 import util.Logging
 import play.api.libs.concurrent.Execution.Implicits._
-import models.CustomFormats._
 
 object Search extends Controller with AuthActions with Logging {
 
@@ -18,7 +17,7 @@ object Search extends Controller with AuthActions with Logging {
     AdminApi.getUsers(searchQuery).map{response =>
       val json = Json.parse(response.body)
       val searchResponse = SearchResponse.create(json)
-      Ok(views.html.searchResults(Messages("searchResults.title"),searchQuery,SearchResult.mockData(), searchResponse.toString))
+      Ok(views.html.searchResults(Messages("searchResults.title"),searchQuery, searchResponse))
   }
   }
 
