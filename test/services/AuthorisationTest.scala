4f8eee36096efc0a893e031bd41dd4250704079a
Susie Coleman <susie.coleman@guardian.co.uk>
2015-10-26 12:46:18 +0000

    Removed $ from filename

--- test/services/AuthorisationTest.scala
+++ test/services/AuthorisationTest.scala
@@ -4,13 +4,27 @@ import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 
-class Authorisation$Test extends FlatSpec with Matchers {
+class AuthorisationTest extends FlatSpec with Matchers {
+
+  val required = Set("2FA_enforce","useradmin")
 
   it should "determine if user is in correct groups" in {
-    val required = Set("2FA_enforce","useradmin")
     Authorisation.isAuthorised(required, Set("2FA_enforce","useradmin")) should be (true)
+  }
+
+  it should "determine order of groups in the authorised set has no effect" in {
     Authorisation.isAuthorised(required, Set("useradmin","2FA_enforce")) should be (true)
+  }
+
+  it should "determine additional groups in authorised set has no effect on authorisation" in {
     Authorisation.isAuthorised(required, Set("2FA_enforce","useradmin","testgroup")) should be (true)
+  }
+
+  it should "determine the absence of the 'useradmin' group prevents authorisation" in {
+    Authorisation.isAuthorised(required, Set("2FA_enforce")) should be (false)
+  }
+
+  it should "determine the absence of the '2FA_enforce' group prevents authorisation" in {
     Authorisation.isAuthorised(required, Set("2FA_enforce")) should be (false)
   }
 }
