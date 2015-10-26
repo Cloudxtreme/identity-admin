63a40d74e7aeb5b067dcbc43f05aad5cbbabc0b0
Susie Coleman <susie.coleman@guardian.co.uk>
2015-10-26 12:05:29 +0000

    Removed unneeded code.

--- app/models/SearchResponse.scala
+++ app/models/SearchResponse.scala
@@ -1,7 +1,6 @@
 package models
 
 import play.api.libs.json._
-import play.api.mvc.{Results, Result}
 import scala.language.implicitConversions
 
 case class SearchResponse(total: Int,
@@ -10,8 +9,4 @@ case class SearchResponse(total: Int,
 
 object SearchResponse {
   implicit val format = Json.format[SearchResponse]
-
-  implicit def searchResponseToResult(searchResponse: SearchResponse): Result =
-    Results.Ok(Json.toJson(searchResponse))
-
 }
