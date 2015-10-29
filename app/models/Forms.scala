dca23f9871edda3c916d404aae31f6497187a486
Susie Coleman <susie.coleman@guardian.co.uk>
2015-10-29 16:51:19 +0000

    Add conversion to Json.

--- app/models/Forms.scala
+++ app/models/Forms.scala
@@ -2,6 +2,7 @@ package models
 
 import play.api.data.{Mapping, Form}
 import play.api.data.Forms._
+import play.api.libs.json.{JsValue, Json}
 
 object Forms {
 
@@ -13,9 +14,24 @@ object Forms {
                                 receiveGnmMarketing: Option[Boolean] = None,
                                 receive3rdPartyMarketing: Option[Boolean] = None)
 
+  object UserUpdateRequest {
+    def convertUserUpdateRequestToJson(userData: UserUpdateRequest): JsValue = {
+      Json.obj(
+        "email" -> userData.email,
+        "username" -> userData.username,
+        "firstName" -> userData.firstName,
+        "lastName" -> userData.lastName,
+        "receiveGnmMarketing" -> userData.receiveGnmMarketing,
+        "receive3rdPartyMarketing" -> userData.receive3rdPartyMarketing
+      )
+    }
+  }
+
+
+//  Experimentation with validation
   private val alphaText: Mapping[String] = text.verifying("First name must be characters only." , name => name.matches("[A-z]+"))
-//  Written validation for username not yet in use
   private val username: Mapping[String] = text.verifying("Invalid display name, ", name => name.matches("[A-z0-9]+") && name.length > 5 && name.length < 21)
+  private val email1: Mapping[String] = text.verifying("valid", name => name.length > 4)
 
   val userForm = Form(
     mapping(
