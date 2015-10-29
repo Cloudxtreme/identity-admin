315a7465a18d30daaace65d13874136fbf1a527b
markjamesbutler <markjamesbutler@gmail.com>
2015-10-29 16:14:52 +0000

    Added HSTS.

---
+++ app/filters/StrictTransportSecurityHeaderFilter.scala
@@ -0,0 +1,16 @@
+package filters
+
+import play.api.mvc.{Filter, RequestHeader, Result}
+
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+object StrictTransportSecurityHeaderFilter extends Filter {
+
+  private val OneYearInSeconds = 31536000
+  private val Header = "Strict-Transport-Security" -> s"max-age=$OneYearInSeconds; preload"
+
+  override def apply(nextFilter: (RequestHeader) => Future[Result])(request: RequestHeader): Future[Result] = {
+    nextFilter(request).map(_.withHeaders(Header))
+  }
+}
