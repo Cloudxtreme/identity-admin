9635bd047ac5134663bdad28c41014aefef53113
Susie Coleman <susie.coleman@guardian.co.uk>
2015-11-02 18:33:12 +0000

    Add logging and authentication.

--- app/controllers/UpdateUser.scala
+++ app/controllers/UpdateUser.scala
@@ -1,9 +1,8 @@
 package controllers
 
-import play.api.mvc.Action
 import javax.inject.Inject
 import models.Forms._
-import models.{UserUpdateRequest, User}
+import models.UserUpdateRequest
 import play.api.i18n.Messages
 import play.api.i18n.Messages.Implicits._
 import play.api.Play.current
@@ -11,26 +10,27 @@ import play.api.mvc.Controller
 import play.api.mvc.Result
 import services.AdminApi
 import play.api.libs.concurrent.Execution.Implicits._
-
+import util.Logging
 import scala.concurrent.Future
 
-trait SaveAction extends Controller{
-
+trait SaveAction extends Controller with Logging{
   val adminApi: AdminApi
 
   private[controllers] def doSave(userId: String, userRequest: UserUpdateRequest, searchQuery: String): Future[Result] = {
     adminApi.updateUserData(userId, userRequest).map {
-      case Right(_) => Redirect(routes.Search.search(searchQuery)).flashing("success" -> "User has been updated")
-      case Left(error) => Redirect(routes.Search.search(searchQuery)).flashing("error" -> error.toString)
+      case Right(_) =>
+        logger.info("Successfully updated user in database. Redirecting to search.")
+        Redirect(routes.Search.search(searchQuery)).flashing("success" -> "User has been updated")
+      case Left(error) =>
+        logger.error(s"Failed to update user. error: $error")
+        Redirect(routes.Search.search(searchQuery)).flashing("error" -> error.toString)
     }
   }
 }
 
 class UpdateUser @Inject() (val adminApi: AdminApi) extends Controller with AuthActions with SaveAction {
 
-  val blankUser = User("","")
-
-  def save(searchQuery: String) = Action.async { request =>
+  def save(searchQuery: String) = AuthAction.async { request =>
     userForm.bindFromRequest()(request).fold(
       errorForm => Future(BadRequest(views.html.editUser(
         Messages("editUser.title"),
