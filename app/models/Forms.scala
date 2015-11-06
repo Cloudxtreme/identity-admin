27f2bb8c9ed62e57919a4cb30e61f727d280eec2
Susie Coleman <susie.coleman@guardian.co.uk>
2015-11-06 10:59:39 +0000

    Neater date formatting in code.

--- app/models/Forms.scala
+++ app/models/Forms.scala
@@ -66,9 +66,9 @@ object Forms {
         "country" -> optional(text),
         "postcode" -> optional(text)
       )(Address.apply)(Address.unapply),
-      "lastActivityDate" -> optional(jodaDate),
+      "lastActivityDate" -> optional(jodaDate("dd-MM-YYYY, HH:mm:ss zzz")),
       "lastActivityIp" -> optional(text),
-      "registrationDate" -> optional(jodaDate),
+      "registrationDate" -> optional(jodaDate("dd-MM-YYYY, HH:mm:ss zzz")),
       "registrationIp" -> optional(text),
       "status" -> mapping(
         "receive3rdPartyMarketing" -> optional(boolean),
@@ -78,7 +78,7 @@ object Forms {
       "groups" -> seq(
         mapping(
           "name" -> text,
-          "joinDate" -> optional(jodaDate)
+          "joinDate" -> optional(jodaDate("dd-MM-YYYY, HH:mm:ss zzz"))
         )(UserGroup.apply)(UserGroup.unapply)
       )
     )(UserForm.apply)(UserForm.unapply)
