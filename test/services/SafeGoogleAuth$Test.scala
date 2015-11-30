91ebc74fd1c68707e2316e775046366b4a0a66c2
markjamesbutler <markjamesbutler@gmail.com>
2015-11-30 21:30:53 +0000

    First passing test.

--- test/services/SafeGoogleAuth$Test.scala
+++ test/services/SafeGoogleAuth$Test.scala
@@ -2,18 +2,18 @@ package services
 
 import auth.CSRFRequest
 import com.gu.googleauth.{UserIdentity, GoogleAuthConfig}
-import org.scalatest.FlatSpec
+import org.scalatest.concurrent.{ScalaFutures, AsyncAssertions}
+import org.scalatest.{ParallelTestExecution, Matchers, FlatSpec}
 import play.api.test.Helpers._
 import play.api.test.{FakeApplication, FakeRequest}
-import play.filters.csrf.CSRFAction
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class SafeGoogleAuth$Test extends FlatSpec {
+class SafeGoogleAuth$Test extends FlatSpec with Matchers with ScalaFutures with ParallelTestExecution with AsyncAssertions {
 
-  val identity = UserIdentity("sub","email","firstName","lastName",1234,None)
-  val googleAuthConfig = GoogleAuthConfig("1", "ssh", "http://oauthcallback.com", None, None, false)
+  val identity = UserIdentity("sub","test.com","firstName","lastName",1234,None)
+  val googleAuthConfig = GoogleAuthConfig("1", "ssh", "http://test.com", Some("test.com"), None, false)
   val request = FakeRequest()
   implicit val app = FakeApplication()
   implicit val csrfAction = new CSRFRequest("token", request)
@@ -22,7 +22,9 @@ class SafeGoogleAuth$Test extends FlatSpec {
 
   it should "validateUserIdentity" in {
     running(app) {
-      SafeGoogleAuth.validateUserIdentity(validatedUserIdentity, googleAuthConfig, "test")
+      whenReady(SafeGoogleAuth.validateUserIdentity(validatedUserIdentity, googleAuthConfig, "test")) { result =>
+        result should be(Right(identity))
+      }
     }
   }
 
