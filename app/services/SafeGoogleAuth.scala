392e63813a66f03e4fd0662a72030286ddc378f5
markjamesbutler <markjamesbutler@gmail.com>
2015-11-30 18:49:29 +0000

    Add Try to capture error outside of Future.

--- app/services/SafeGoogleAuth.scala
+++ app/services/SafeGoogleAuth.scala
@@ -7,6 +7,7 @@ import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.duration._
 
 import scala.concurrent.{Await, Future}
+import scala.util.Try
 
 object SafeGoogleAuth {
 
@@ -15,20 +16,22 @@ object SafeGoogleAuth {
   def correctHostedDomain(identity: UserIdentity): Boolean = Some(identity.emailDomain) == authConfig.domain
 
   def validateUserIdentity[A](implicit request: CSRFRequest[A]): Future[Either[LoginError, UserIdentity]] = {
-    GoogleAuth.validatedUserIdentity(GoogleAuthConf.googleAuthConfig, request.csrfToken).map {
+    val t = Try(GoogleAuth.validatedUserIdentity(GoogleAuthConf.googleAuthConfig, request.csrfToken).map {
       case identity: UserIdentity if correctHostedDomain(identity) => Right(identity)
       case identity: UserIdentity => Left(DomainValidationFailed())
       case _ => Left(IdentityValidationFailed())
     }.recover {
-      case ex: IllegalArgumentException => Left(CSRFValidationFailed())
       case _ => Left(IdentityValidationFailed())
+    }).recover {
+      case e: IllegalArgumentException => Future(Left(CSRFValidationFailed()))
     }
+    t.get
   }
 
   def validateUser[A](implicit request: CSRFRequest[A]): Future[Either[LoginError, UserIdentity]] = {
     validateUserIdentity.map {
       case Right(identity: UserIdentity) => Await.result(GoogleGroups.isUserAdmin(identity), 5.seconds)
-      case Left => Left(IdentityValidationFailed())
+      case _ => Left(IdentityValidationFailed())
     }
   }
 }
