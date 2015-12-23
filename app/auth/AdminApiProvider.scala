54abbd1eaa42ebad6ca05e41cbad09697b323ddf
Emmanuelle Poirier <emmanuelle.poirier@guardian.co.uk>
2015-12-23 17:26:48 +0000

    Instantiate API only once

--- app/auth/AdminApiProvider.scala
+++ app/auth/AdminApiProvider.scala
@@ -9,6 +9,10 @@ import services.AdminApi
 // It ensures that no access to the api will be accidentally provided if a request is unauthenticated
 trait AdminApiProvider { self: Controller => // `self: Controller` ensures the trait can only be mixed in controllers
   def adminApi(implicit request: AuthenticatedRequest[_, UserIdentity]): AdminApi = {
-    new AdminApi
+    AdminApiProvider.adminApi
   }
+}
+
+object AdminApiProvider {
+  lazy val adminApi = new AdminApi
 }
